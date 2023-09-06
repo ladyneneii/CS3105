18a73512aa79465d9ce1452fa6b35276d2655eec
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-09-06 20:51:17 +0800

    Added starter files and components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Message from "./components/Message";
+import Card from "./components/Card"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Message />
+      <Card />
+    </div>
+  );
+}
+
+export default App
