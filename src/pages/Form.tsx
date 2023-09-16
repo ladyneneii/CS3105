d1b0e40f771bef3a1c12f497e605213f7ef5cae7
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-09-17 00:46:06 +0800

    Added shimmer with correct dimensions

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Skeleton } from "@mui/material";
 import { userSchema } from "../validations/UserValidation";
+import "../index.css";
 
 const Form = () => {
   const createUser = async (e: any) => {
@@ -13,23 +14,43 @@ const Form = () => {
     };
 
     const isValid = await userSchema.isValid(formData);
+    console.log(isValid);
   };
 
   const [isLoading, setIsLoading] = useState(false);
+  // const [inputDimensions, setInputDimensions] = useState({
+  //   width: 0,
+  //   height: 0,
+  // });
+  // const inputRef = useRef<HTMLInputElement | null>(null);
+
+  // useEffect(() => {
+  //   if (inputRef.current) {
+  //     const { width, height } = inputRef.current.getBoundingClientRect();
+  //     setInputDimensions({ width, height });
+  //     console.log(inputDimensions.width, inputDimensions.height);
+  //   }
+  // }, []);
 
   return (
     <>
       <form onSubmit={createUser}>
         {isLoading ? (
           <div className="mb-3">
-            <input type="text" className="form-control" placeholder="Name..." />
+            <input
+              type="text"
+              className="form-control form__input"
+              placeholder="Name..."
+              style={{ width: "400px" /* other CSS properties here */ }}
+              // ref={inputRef}
+            />
           </div>
         ) : (
           <Skeleton
             variant="rectangular"
             animation="wave"
-            width={345}
-            height={100}
+            width={400}
+            height={38}
           />
         )}
         <div className="mb-3">
@@ -37,6 +58,7 @@ const Form = () => {
             type="text"
             className="form-control"
             placeholder="name@example.com"
+            style={{ width: "400px" /* other CSS properties here */ }}
           />
         </div>
         <div className="mb-3">
@@ -44,6 +66,7 @@ const Form = () => {
             type="text"
             className="form-control"
             placeholder="password123"
+            style={{ width: "400px" /* other CSS properties here */ }}
           />
         </div>
         <input type="submit" />
