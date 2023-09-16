6408ecabf1b33824094f81d96b7849d1c95f54a4
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-09-16 21:53:05 +0800

    Added form

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,7 +1,32 @@
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <>
+      <form>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name..."
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="password123"
+          />
+        </div>
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
 
-export default Form
+export default Form;
