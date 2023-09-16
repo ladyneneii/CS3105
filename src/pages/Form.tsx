a9a7ed223a71866627772095037ab6057010eef4
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-09-16 22:20:11 +0800

    Added validation

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,13 +1,24 @@
+import { userSchema } from "../validations/UserValidation";
+
 const Form = () => {
+  const createUser = async (e) => {
+    e.preventDefault();
+
+    let formData = {
+      name: e.target[0].value,
+      email: e.target[1].value,
+      password: e.target[2].value,
+    };
+
+    const isValid = await userSchema.isValid(formData);
+    console.log(isValid);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={createUser}>
         <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Name..."
-          />
+          <input type="text" className="form-control" placeholder="Name..." />
         </div>
         <div className="mb-3">
           <input
